5f356d30022e4f1b02ef03e046cb92ff90282cb8
cristianderudi <skorpiano@gmail.com>
2023-08-11 20:45:40 -0300

    Segundo ejercicio resuelto

--- Ej2.js
+++ Ej2.js
@@ -1,1 +1,13 @@
 /* Imprimir los números entre el 5 y el 20, saltando de tres en tres. */
+
+// Inicializar una variable con el valor de inicio (5)
+var numero = 5;
+
+// Usar un bucle while para imprimir números entre 5 y 20, saltando de tres en tres
+while (numero <= 20) {
+  // Imprimir el valor actual de 'numero'
+  console.log(numero);
+
+  // Incrementar 'numero' en 3 para saltar de tres en tres
+  numero += 3;
+}

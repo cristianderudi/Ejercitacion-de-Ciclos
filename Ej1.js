c74baff7a6a48bb58ead058bc3e11c6eeaffb00a
cristianderudi <skorpiano@gmail.com>
2023-08-11 20:27:24 -0300

    Primer Ejercicio Resuelto

--- Ej1.js
+++ Ej1.js
@@ -1,2 +1,10 @@
 /* Crear una programa que pida un valor y muestre los 10
 números siguientes. */
+
+const prompt = require("prompt-sync")({ sigint: true});
+
+let num = prompt("Introduce un número:"); // Pide al usuario que introduzca un número y lo guarda en la variable "num"
+num = parseInt(num); // Convierte el valor introducido por el usuario a un número entero
+for (let i = 1; i <= 10; i++) { // Crea un bucle que se ejecutará 10 veces
+    console.log(num + i); // Muestra en la consola el valor de "num" más el valor de "i" en cada iteración del bucle
+}

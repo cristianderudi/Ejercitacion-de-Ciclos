cf6d37a4a3b86fe86191f72a29fbffaa6e6bc2a3
cristianderudi <skorpiano@gmail.com>
2023-08-11 21:13:08 -0300

    Tercer ejercicio resuelto

--- Ej3.js
+++ Ej3.js
@@ -1,2 +1,14 @@
 /* Escribir un programa que muestre la sumatoria de todos los números
 entre el 0 y el 100 */
+
+// Inicializar una variable para almacenar la sumatoria
+var sumatoria = 0;
+
+// Utilizar un bucle for para recorrer los números del 0 al 100
+for (var i = 0; i <= 100; i++) {
+  // Sumar el valor actual de 'i' a la sumatoria existente
+  sumatoria += i;
+}
+
+// Imprimir la sumatoria en la consola
+console.log("La sumatoria es: " + sumatoria);

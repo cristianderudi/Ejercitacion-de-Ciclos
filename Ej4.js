1734ed6fd3d03e4de6672c8ac9a0eb8798eb09d4
cristianderudi <skorpiano@gmail.com>
2023-08-11 22:50:23 -0300

    Cuarto ejercicio resuelto

--- Ej4.js
+++ Ej4.js
@@ -1,3 +1,23 @@
 /* Dado un número entero positivo, mostrar su factorial. El factorial de
 un número se obtiene multiplicando todos los números enteros
 positivos que hay entre el 1 y ese número. */
+
+const prompt = require("prompt-sync")({ sigint: true});
+// Pedir al usuario que ingrese un número entero positivo
+var numero = parseInt(prompt("Ingresa un número entero positivo: "));
+
+// Validar que el número ingresado sea positivo
+if (numero < 0) {
+  console.log("Ingresa un número entero positivo válido.");
+} else {
+  // Inicializar una variable para almacenar el factorial
+  var factorial = 1;
+
+  // Calcular el factorial del número ingresado
+  for (var i = 1; i <= numero; i++) {
+    factorial *= i;
+  }
+
+  // Mostrar el factorial en la consola
+  console.log("El factorial de " + numero + " es: " + factorial);
+}

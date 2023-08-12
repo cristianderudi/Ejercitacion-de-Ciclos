05fae0505827d5c56fa786d5c08490e6f04068bd
cristianderudi <skorpiano@gmail.com>
2023-08-12 20:54:27 -0300

    Octavo ejercicio resuelto

--- Ej8.js
+++ Ej8.js
@@ -3,3 +3,27 @@ letras que restan para llegar a diciembre del mismo año (por ejemplo, si ingres
 octubre, noviembre y diciembre). Validar que se ingrese un número de mes válido y el proceso
 finaliza cuando el operador ingresa el valor cero. Muestre también los valores correctos e
 incorrectos ingresados por el usuario. */
+
+const prompt = require("prompt-sync")({ sigint: true});
+var meses = [
+    "enero", "febrero", "marzo", "abril", "mayo", "junio",
+    "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"
+  ];
+  
+  var mesIngresado;
+  var mesesRestantes = [];
+  
+  do {
+    mesIngresado = parseInt(prompt("Ingresa un número de mes (1-12) o 0 para salir: "));
+  
+    if (mesIngresado >= 1 && mesIngresado <= 12) {
+      mesesRestantes = meses.slice(mesIngresado - 1);
+      console.log("Meses restantes hasta diciembre: " + mesesRestantes.join(", "));
+    } else if (mesIngresado !== 0) {
+      console.log("Valor ingresado no válido.");
+    }
+  
+  } while (mesIngresado !== 0);
+  
+  console.log("Valores ingresados correctamente: " + mesesRestantes.join(", "));
+  
